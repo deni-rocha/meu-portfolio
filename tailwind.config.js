22944d0b8e24a1bec92ba3acb943a0cdb621c3cd
Denilson Rocha <denilsonrocha_@outlook.com>
2023-01-28 11:32:58 -0300

    configurações e temas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,56 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      screens: {
-        xs: { max: '600px' },
-        sm: '320px',
-        ...defaultTheme.screens
-      },
+      // screens: {
+      //   lgExtra: { min: "1118px" },
+      //   ...defaultTheme.screens
+      // },
       colors: {
+        background: "#11172B",
+        backgroundLight: "#1B2138",
+        primary: "#0EE7B7",
+        secondary: "#7AC7E3",
+        gradient: "linear-gradient(225deg, #313860 0%, #11172B 100%);",
+        text: "#848DA0",
+        textHighlight: "#444B5B",
+        textLight: "#C4C4C4",
+        border: "#313958",
+        inputBackground: "#1E253E",
         ...defaultTheme.colors
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        jetBrains: ["JetBrains Mono", "monospace"]
+      },
+      animation: {
+        rightIn: "rightIn .5s",
+        leftIn: "leftIn .6s",
+        pulse: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1)"
+      },
+      keyframes: {
+        rightIn: {
+          from: {
+            right: "-333px"
+          },
+          to: {
+            right: "0px"
+          }
+        },
+        leftIn: {
+          from: {
+            left: "-333px"
+          },
+          to: {
+            left: "0px"
+          }
+        }
       }
     }
   },
   plugins: []
 }
+
