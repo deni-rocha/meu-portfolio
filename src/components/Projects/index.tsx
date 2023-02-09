39db056b630add00469c689f377f2b99598f4b61
Denilson Rocha <denilsonrocha_@outlook.com>
2023-02-08 21:34:11 -0300

    remove a div que agora está no componente Main

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -6,20 +6,21 @@ const Projects = (): JSX.Element => {
   const [projects] = useState(dataProjects)
 
   return (
-    <div className="flex flex-col items-center mt-36 md:mt-20 lg:mt-12 lg:items-start">
+    <>
       {projects.map((item) => {
         return (
           <CardProject
             type={item.type}
             slug={item.id}
-            link={item.link}
+            linkWeb={item.linkWeb}
+            linkCode={item.linkCode}
             imgUrl={item.thumbnail}
             title={item.title}
             key={item.id}
           />
         )
       })}
-    </div>
+    </>
   )
 }
 
