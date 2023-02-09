fcf0eacbcbe5d3c699681fb7ef81a246c5d37db1
Denilson Rocha <denilsonrocha_@outlook.com>
2023-02-08 21:34:28 -0300

    adiciona o Main na Home do projeto

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import Footer from "../../components/Footer"
 import Header from "../../components/Header"
-import Projects from "../../components/Projects"
+import Main from "../../components/Main"
 
 const Home = (): JSX.Element => {
   return (
     <div className="bg-background text-textLight">
       <div className="w-11/12 mx-auto md:w-3/4">
         <Header />
-        <Projects />
+        <Main />
         <Footer />
       </div>
     </div>
